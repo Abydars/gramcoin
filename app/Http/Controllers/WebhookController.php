fdd51e2fd782b6891bb658bfec8e2bc8f27ee94e
AbydArs <abidr.w@gmail.com>
2017-12-15 20:17:57 +0500

    create transaction history

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -2,12 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Transaction;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
+use Currency;
 
 class WebhookController extends Controller
 {
 	public function transactionEvent( $id, Request $request )
 	{
-		file_put_contents( storage_path( 'logs' ) . '/webhook-' . $id . '.txt', json_encode( $request->all() ) );
+		$event_type = $request->get( 'event_type' );
+		$data       = $request->get( 'data' );
+		$wallet     = $request->get( 'wallet' );
+
+		switch ( $event_type ) {
+			case "address-transactions":
+				$transaction = Transaction::firstOrCreate( [
+					                                           'tx_hash'       => $data['hash'],
+					                                           'recipient'     => $data['outputs'][0]['address'],
+					                                           'direction'     => 'received',
+					                                           'amount'        => Currency::convertToBtc( $data['estimated_value'] ),
+					                                           'confirmations' => $data['confirmations'],
+					                                           'wallet_id'     => $wallet['identifier'],
+					                                           'tx_time'       => Carbon::now()
+				                                           ] );
+				if ( $transaction->id > 0 ) {
+					return response()->json( [
+						                         'code'    => 200,
+						                         'message' => 'Transaction ID: ' . $transaction->id
+					                         ] );
+				} else {
+					return response()->json( [
+						                         'code'    => 400,
+						                         'message' => 'Failed to create transaction'
+					                         ] );
+				}
+				break;
+		}
 	}
 }
