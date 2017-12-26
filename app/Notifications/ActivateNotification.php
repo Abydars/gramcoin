076520c232fe395bd86bef107ad307dc6fc65657
AbydArs <abidr.w@gmail.com>
2017-12-27 04:17:02 +0500

    test email

--- app/Notifications/ActivateNotification.php
+++ app/Notifications/ActivateNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class ActivateNotification extends Notification implements ShouldQueue
+class ActivateNotification extends Notification
 {
 	use Queueable;
 
