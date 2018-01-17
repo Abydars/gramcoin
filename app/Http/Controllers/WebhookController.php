c2bda7df7cfe5e63ba8f10740a1b1282fb2b8829
AbydArs <abidr.w@gmail.com>
2018-01-18 04:10:04 +0500

    update wallet base logic

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Transaction;
+use App\UserAddress;
 use App\UserWallet;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -11,7 +12,7 @@ use Currency;
 
 class WebhookController extends Controller
 {
-	public function transactionEvent( $identifier, Request $request )
+	public function _transactionEvent( $identifier, Request $request )
 	{
 		$event_type      = $request->get( 'event_type' );
 		$data            = $request->get( 'data' );
@@ -97,6 +98,101 @@ class WebhookController extends Controller
 					}
 					break;
 			}
+		}
+	}
+
+	public function transactionEvent( $identifier, Request $request )
+	{
+		$event_type      = $request->get( 'event_type' );
+		$data            = $request->get( 'data' );
+		$response_wallet = $request->get( 'wallet' );
+
+		switch ( $event_type ) {
+			case "address-transactions":
+
+				$addresses = $response_wallet['addresses'];
+
+				if ( count( $addresses ) > 1 ) {
+					$log = date( "Y-m-d h:i:s" ) . ' : ' . $identifier . ' : ' . json_encode( $request->all() ) . PHP_EOL;
+					file_put_contents( storage_path( 'logs' ) . '/MultipleAddressesTrans.txt', $log, FILE_APPEND );
+
+					return;
+				}
+
+				$address = UserAddress::with( 'user' )->where( 'address', $addresses[0] )->first();
+				$user    = $address->user;
+
+				$log = date( "Y-m-d h:i:s" ) . ' : ' . $identifier . ' : ' . json_encode( $request->all() ) . PHP_EOL;
+				file_put_contents( storage_path( 'logs' ) . '/transactions.txt', $log, FILE_APPEND );
+
+				$transaction = Transaction::where( 'tx_hash', $data['hash'] )
+				                          ->where( 'wallet_id', $user->wallet_id );
+
+				$confirmed        = $data['confirmations'] > 0;
+				$was_confirmed    = $transaction->exists() && $transaction->first()->status == 'confirmed';
+				$wallet_addresses = $addresses;
+				$address          = false;
+				$amount           = false;
+				$is_sender        = false;
+				$has_output       = false;
+
+				foreach ( $data['inputs'] as $input ) {
+					$is_sender = in_array( $input['address'], $wallet_addresses );
+
+					$output_index = 0;//$input['output_index'];
+					$amount       = $data['outputs'][ $output_index ]['value'];
+					$address      = $data['outputs'][ $output_index ]['address'];
+					$has_output   = in_array( $address, $wallet_addresses );
+
+					if ( $has_output ) {
+						break;
+					}
+				}
+
+				if ( ! $is_sender && ! $has_output ) {
+					return;
+				}
+
+				$txData = [
+					'tx_hash'       => $data['hash'],
+					'recipient'     => $address,
+					'direction'     => $is_sender ? 'sent' : 'receiver',
+					'amount'        => $amount,
+					'confirmations' => $data['confirmations'],
+					'status'        => $confirmed ? 'confirmed' : 'unconfirmed',
+					'wallet_id'     => $user->wallet_id,
+					'tx_time'       => Carbon::now()->toDateTimeString()
+				];
+
+				$transaction = Transaction::updateOrCreate( [
+					                                            'tx_hash'   => $data['hash'],
+					                                            'wallet_id' => $user->wallet_id
+				                                            ], $txData );
+
+				if ( $transaction->id > 0 ) {
+
+					if ( $confirmed && ! $was_confirmed ) {
+
+						if ( $is_sender ) {
+							$user->btc_balance -= $amount;
+						} else {
+							$user->btc_balance += $amount;
+						}
+
+						$user->save();
+					}
+
+					return response()->json( [
+						                         'code'    => 200,
+						                         'message' => 'Transaction ID: ' . $transaction->id
+					                         ] );
+				} else {
+					return response()->json( [
+						                         'code'    => 400,
+						                         'message' => 'Failed to create transaction'
+					                         ] );
+				}
+				break;
 		}
 	}
 }
